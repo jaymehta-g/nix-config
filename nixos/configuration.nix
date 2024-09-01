1834bc80c17fbbc167f0bd6b2a5c6412e11e631d
jay-nixos-vm <wjayww@gmail.com>
2024-09-01 12:11:37 -0500

    added pkgs

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -88,7 +88,6 @@
     extraGroups = [ "networkmanager" "wheel" ];
     packages = with pkgs; [
       kdePackages.kate
-      git
     #  thunderbird
     ];
   };
@@ -107,7 +106,9 @@
   # $ nix search wget
   environment.systemPackages = with pkgs; [
   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #  wget
+    wget
+    git
+    vscode
   ];
 
   # Some programs need SUID wrappers, can be configured further or are
