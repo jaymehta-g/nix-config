ebc254e6ede3297cdee236f3b145c59534cce565
jay-nixos-vm <wjayww@gmail.com>
2024-09-11 21:36:53 -0500

    14 current  2024-09-11 21:36:51  24.05.20240910.44a71ff   6.6.50                          *

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -66,12 +66,6 @@
     alsa.enable = true;
     alsa.support32Bit = true;
     pulse.enable = true;
-    # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
-
-    # use the example session manager (no others are packaged yet so this is enabled by default,
-    # no need to redefine it in your config for now)
-    #media-session.enable = true;
   };
 
   # Enable touchpad support (enabled default in most desktopManager).
@@ -107,6 +101,7 @@
     libsForQt5.qtstyleplugin-kvantum
     kdePackages.kdialog
     git
+    gitg
     wget
     vscode
     home-manager
@@ -114,25 +109,6 @@
     unstable.python3
     cargo
   ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
 
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
