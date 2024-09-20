47c4fdd5c4294cc945e751c89355a1109babeacc
jay-desktop-nix <91770194+jaymehta-g@users.noreply.github.com>
2024-09-19 23:30:13 -0500

    16 current  2024-09-19 23:30:10  24.05.20240916.086b448   6.6.51                          *

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -121,6 +121,8 @@
     discord
   ];
 
+  gaming.enable = true;
+
   # This value determines the NixOS release from which the default
   # settings for stateful data, like file locations and database versions
   # on your system were taken. It‘s perfectly fine and recommended to leave
