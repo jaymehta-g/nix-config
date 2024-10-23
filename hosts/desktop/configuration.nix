9d1259b7c0e832d7e448e9b266bf654fdc2e3c29
jay-desktop <91770194+jaymehta-g@users.noreply.github.com>
2024-10-23 12:13:59 -0500

    49 current  2024-10-23 12:13:58  24.05.20241017.4eb33fe  6.6.57                          *

--- hosts/desktop/configuration.nix
+++ hosts/desktop/configuration.nix
@@ -73,14 +73,40 @@
   };
 
   # popping and crackling fix
-  services.pipewire.extraConfig.pipewire."92-low-latency" = {
-    "context.properties" = {
-      "default.clock.rate" = 48000;
-      "default.clock.quantum" = 512;
-      "default.clock.min-quantum" = 256;
-      "default.clock.max-quantum" = 1024;
+  services.pipewire.extraConfig = {
+    pipewire."92-low-latency" = {
+      "context.properties" = {
+        "default.clock.rate" = 48000;
+        "default.clock.quantum" = 512;
+        "default.clock.min-quantum" = 256;
+        "default.clock.max-quantum" = 1024;
+      };
     };
+    # for games apparently? from https://github.com/joinemm/empire/blob/master/modules/desktop/sound.nix#L23
+    pipewire-pulse."92-quantum" = 
+      let
+        qr = "256/48000";
+      in
+      {
+        "context.properties" = [
+          {
+            name = "libpipewire-module-protocol-pulse";
+            args = { };
+          }
+        ];
+        "pulse.properties" = {
+          "pulse.default.req" = qr;
+          "pulse.min.req" = qr;
+          "pulse.max.req" = qr;
+          "pulse.min.quantum" = qr;
+          "pulse.max.quantum" = qr;
+        };
+        "stream.properties" = {
+          "node.latency" = qr;
+        };
+      };
   };
+  
 
   users.groups.nix-manager = {};
   users.users.jay = {
