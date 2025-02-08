7c5680be3c14822c352f733571a0cce02494366c
jay-desktop <91770194+jaymehta-g@users.noreply.github.com>
2025-02-08 12:17:26 -0600

    93 current  2025-02-08 12:17:24  24.11.20250204.030ba19  6.6.75                          *

--- mods/normcap.nix
+++ mods/normcap.nix
@@ -6,6 +6,9 @@
     };
 
     config = lib.mkIf config.normcap.enable {
-        environment.systemPackages = [ pkgs.normcap ];
+        environment.systemPackages = [ 
+            pkgs.normcap
+            pkgs.xsel
+        ];
     };
 }
