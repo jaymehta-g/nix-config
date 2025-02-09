1149ab622ff3d21bc27ac0ad21013524c2f63543
jay-desktop <91770194+jaymehta-g@users.noreply.github.com>
2025-02-08 23:33:14 -0600

    99 current  2025-02-08 23:33:11  24.11.20250206.f5a32fa  6.6.75                          *

--- mods/gnome-desktop.nix
+++ mods/gnome-desktop.nix
@@ -19,7 +19,18 @@
             gnomeExtensions.compiz-windows-effect
             gnomeExtensions.compiz-alike-magic-lamp-effect
             gnomeExtensions.blur-my-shell
-            gnomeExtensions.dock-from-dash
+            # gnomeExtensions.dock-from-dash
+            # (gnomeExtensions.buildGnomeExtension {
+            #     uuid="dash-to-dock@micxgx.gmail.com";
+            #     pname="dash-to-dock";
+            #     name="Dash to Dock";
+            #     description=".";
+            #     link="https://micheleg.github.io/dash-to-dock/";
+            #     version=99;
+            #     sha256="";
+            #     metadata="ew0KInNoZWxsLXZlcnNpb24iOiBbDQogICAgIjQ1IiwNCiAgICAiNDYiLA0KICAgICI0NyIsDQogICAgIjQ4Ig0KXSwNCiJ1dWlkIjogImRhc2gtdG8tZG9ja0BtaWN4Z3guZ21haWwuY29tIiwNCiJuYW1lIjogIkRhc2ggdG8gRG9jayIsDQoiZGVzY3JpcHRpb24iOiAiQSBkb2NrIGZvciB0aGUgR25vbWUgU2hlbGwuIFRoaXMgZXh0ZW5zaW9uIG1vdmVzIHRoZSBkYXNoIG91dCBvZiB0aGUgb3ZlcnZpZXcgdHJhbnNmb3JtaW5nIGl0IGluIGEgZG9jayBmb3IgYW4gZWFzaWVyIGxhdW5jaGluZyBvZiBhcHBsaWNhdGlvbnMgYW5kIGEgZmFzdGVyIHN3aXRjaGluZyBiZXR3ZWVuIHdpbmRvd3MgYW5kIGRlc2t0b3BzLiBTaWRlIGFuZCBib3R0b20gcGxhY2VtZW50IG9wdGlvbnMgYXJlIGF2YWlsYWJsZS4iLA0KIm9yaWdpbmFsLWF1dGhvciI6ICJtaWN4Z3hAZ21haWwuY29tIiwNCiJ1cmwiOiAiaHR0cHM6Ly9taWNoZWxlZy5naXRodWIuaW8vZGFzaC10by1kb2NrLyIsDQoiZ2V0dGV4dC1kb21haW4iOiAiZGFzaHRvZG9jayIsDQoidmVyc2lvbiI6IDk5DQp9";
+            # })
+            gnome47Extensions."dash-to-dock@micxgx.gmail.com"
             gnomeExtensions.middle-click-to-close-in-overview
         ];
         # https://discourse.nixos.org/t/howto-disable-most-gnome-default-applications-and-what-they-are/13505
