d680e0a24587f6dc48fc2edb643b3e4bcfcb5aa8
Jay <91770194+jaymehta-g@users.noreply.github.com>
2025-10-24 11:49:57 -0500

    220 current  2025-10-24 11:49:55  25.05.20251021.481cf55  6.12.54                          *

--- mods/programs/thunar.nix
+++ mods/programs/thunar.nix
@@ -8,6 +8,7 @@
     config = lib.mkIf config.thunar-fm.enable {
         environment.systemPackages = [
             pkgs.xfce.thunar
+            pkgs.xfce.xfconf
         ];
         services.dbus.packages = with pkgs; [
             xfce.xfconf
