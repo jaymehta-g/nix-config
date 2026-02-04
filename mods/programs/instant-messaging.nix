2a4e85f494927538a2d26d3396c02b8189f6d107
Jay <91770194+jaymehta-g@users.noreply.github.com>
2026-02-03 18:22:35 -0600

    ❄️ desktop Gen 250 on NixOS version 25.11.20260128.fa83fd8

--- mods/programs/instant-messaging.nix
+++ mods/programs/instant-messaging.nix
@@ -8,7 +8,7 @@
     config = lib.mkIf config.instant-messaging.enable {
         environment.systemPackages = with pkgs; [
           signal-desktop
-          discord
+          vesktop
           telegram-desktop
         ];
     };
