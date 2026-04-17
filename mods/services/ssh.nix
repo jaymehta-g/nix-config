d74e9a8ee91d322b545d780dd684720b347f102d
Jay <91770194+jaymehta-g@users.noreply.github.com>
2026-04-16 20:03:45 -0500

    ❄️ lenovo Gen 38 on NixOS version 25.11.20260313.3e20095

--- mods/services/ssh.nix
+++ mods/services/ssh.nix
@@ -24,20 +24,21 @@
             port = 22;
             openFirewall = true;
         };
-    } // lib.mkIf config.ssh.dumbpipe.enable {
-        systemd.services.dumbpipe-ssh-tunnel = {
-            wantedBy = [ "multi-user.target" ];
-            after = [ "network.target" ];
-            serviceConfig = 
-            let
-                tmux = "${pkgs.tmux}/bin/tmux";
-                dp = "${unstable.dumbpipe}/bin/dumbpipe";
-            in
-            {
-                Type = "forking";
-                ExecStart = "${tmux} new -ds dumbpipe-ssh ${dp} listen-tcp --host localhost:1234";
-                ExecStop = "${tmux} kill-session -t dumbpipe-ssh";
-            };
-        };
     };
+    # // lib.mkIf config.ssh.dumbpipe.enable {
+    #     systemd.services.dumbpipe-ssh-tunnel = {
+    #         wantedBy = [ "multi-user.target" ];
+    #         after = [ "network.target" ];
+    #         serviceConfig = 
+    #         let
+    #             tmux = "${pkgs.tmux}/bin/tmux";
+    #             dp = "${unstable.dumbpipe}/bin/dumbpipe";
+    #         in
+    #         {
+    #             Type = "forking";
+    #             ExecStart = "${tmux} new -ds dumbpipe-ssh ${dp} listen-tcp --host localhost:1234";
+    #             ExecStop = "${tmux} kill-session -t dumbpipe-ssh";
+    #         };
+    #     };
+    # };
 }
