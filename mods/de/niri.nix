523b21b0f0877aa14ec69bc13e0fe25cc1e19b3d
Jay <91770194+jaymehta-g@users.noreply.github.com>
2026-09-17 20:49:54 -0500

    new noctalia

--- mods/de/niri.nix
+++ mods/de/niri.nix
@@ -15,7 +15,7 @@ in
 
   config = lib.mkIf config.mods."${opt_name}".enable {
     environment.systemPackages = with pkgs; [
-      unstable.noctalia-shell
+      unstable.noctalia
       alacritty
       fuzzel
       xwayland-satellite
